70b56765fa5ed089cde395e272412d7275d83a27
Dedi <shuffle.ddie@gmail.com>
2021-03-24 22:55:36 +0700

    Task Looping

---
+++ taskLoop.js
@@ -0,0 +1,30 @@
+function naikSatu() {
+    var start = 1;
+    while (start <= 15) {
+        console.log(start);
+        start++;
+    }
+}
+naikSatu();
+console.log("1. Naik Satu");
+function kuadrat() {
+    var start = 1;
+    while (start <= 15) {
+        var nilai = start * start;
+        console.log(nilai)
+        start++;
+    }
+}
+kuadrat();
+console.log("2. kuadrat");
+function kelipatanTiga() {
+    var start = 1;
+    var nilai = 0;
+    while (start <= 20) {
+        nilai = nilai + 3;
+        console.log(nilai);
+        start++;
+    }
+}
+kelipatanTiga();
+console.log("1. Kelipatan 3");
