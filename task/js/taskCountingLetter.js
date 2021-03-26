9de0b509061154fa9b16b64c79023d81e195fb3c
dedisetiawan05 <shuffle.ddie@gmail.com>
2021-03-26 12:37:25 +0700

    task counting letter done

---
+++ task/js/taskCountingLetter.js
@@ -0,0 +1,7 @@
+function cari(){
+    let kalimat = document.getElementById("kalimat").value;
+    let karakter = document.getElementById("karakter").value;
+    var x =kalimat.split(karakter).length-1;
+    console.log(x);
+    document.getElementById("hasil").innerHTML = `Total Huruf/Kata ${karakter} diulang sebanyak ${x} kali`
+}
