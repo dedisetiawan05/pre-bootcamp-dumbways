12f3795c7a4fec309562ef84701fd7fd4de18a21
Dedi <shuffle.ddie@gmail.com>
2021-03-31 16:12:19 +0700

    task fibonacci sequence

---
+++ js/fibonacci.js
@@ -0,0 +1,15 @@
+function fibonacciSequence() {
+    let limit = parseInt(document.getElementById("limit").value);
+    let fibonacci = [];
+    let number;
+    for (let i = 0; i <= limit; i++){
+        if (i < 2) {
+            number = i;
+        } else {
+            number = fibonacci[i - 1] + fibonacci[i - 2];
+        }
+
+        fibonacci.push(number);
+    }
+    document.getElementById("result").innerHTML = `<hr>Result<br>${fibonacci}`
+}
