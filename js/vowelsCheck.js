a2ee35b417da89028e2151cd7c01317e9b7f31db
Dedi <shuffle.ddie@gmail.com>
2021-04-01 04:38:51 +0700

    flowchart vowels check final

--- js/vowelsCheck.js
+++ js/vowelsCheck.js
@@ -1,22 +1,10 @@
 function vowelsCheck() {
     let aplhabet = document.getElementById("alphabet").value;
-    let isVocal;
-    switch (aplhabet) {
-        case "a":
-        case "A":
-        case "e":
-        case "E":
-        case "i":
-        case "I":
-        case "o":
-        case "O":
-        case "u":
-        case "U":
-            isVocal = "Huruf Vokal";
-            break;
-        default:
-            isVocal = "Bukan Huruf Vocal";
-            break;
-    }
+    let alphabetUppercase = aplhabet.toUpperCase();
+    let isVocal = alphabetUppercase  == "A"
+                || alphabetUppercase == "E"
+                || alphabetUppercase == "I"
+                || alphabetUppercase == "O"
+                || alphabetUppercase == "U" ? "Huruf Vokal" : "Bukan Huruf Vocal";
     document.getElementById("result").innerHTML = `<hr>Result<br>Huruf ${aplhabet} adalah ${isVocal}`
 }
