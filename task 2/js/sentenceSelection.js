cde839516431c456ac016a7940bde3e2155e83b2
Dedi <shuffle.ddie@gmail.com>
2021-04-12 07:46:35 +0700

    task Two Done

---
+++ task 2/js/sentenceSelection.js
@@ -0,0 +1,12 @@
+function searchSentence(){
+    let paragraph = document.getElementById("paragraph").value;
+    let sentence = document.getElementById("sentence").value;
+    paragraph = paragraph.split(".");
+    console.log(paragraph);
+    const newParagraph = paragraph.filter(likesentence => likesentence.includes(sentence));
+    // const newParagraph = paragraph.filter(function (likesentence) {
+    //     return likesentence.includes(sentence);
+    // });
+    console.log(newParagraph);
+    document.getElementById("result").innerHTML = `<hr><h3>result</h3>${newParagraph}`;
+}
